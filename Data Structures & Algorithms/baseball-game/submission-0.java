ac9ac26e2b52e6df9edcbc2e5823ae35fb398264
David Reyes Alés <100354169+davra02@users.noreply.github.com>
2026-09-08 17:13:14 +0200

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/baseball-game/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int calPoints(String[] operations) {
+    
+        Stack<Integer> stack = new Stack<Integer>();
+        for(String current: operations){
+            switch(current){
+            case "+":
+                int aux = stack.peek();
+                stack.pop();
+                int n = stack.peek();
+                stack.push(aux);
+                stack.push(aux + n);
+                break;
+            case "D":
+                stack.push(stack.peek()*2);
+                break;
+            case "C":
+                stack.pop();
+                break;
+            default:
+                stack.push(Integer.parseInt(current)); 
+                break;
+        }
+        }
+        
+
+        int result = 0;
+        while(!stack.isEmpty()){
+            result += stack.peek();
+            stack.pop();
+        }
+        return result;
+    }
+}
